0514a39b0fd2c4c65956faef133d134a41491343
error-coder <113674396+error-coder@users.noreply.github.com>
2022-10-25 18:40:51 +0900

    Add files via upload

---
+++ 108.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 왼쪽 먼저 방문 후 자기 자신 처리, 오른족 방문 -> Inorder Traversal
+# 0이 root노드
+from logging import root
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return node
